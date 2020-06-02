8deed0c68d4e0cad75c32ce6cb9cbcdee84df00e
Ryan Inman <inman006@gmail.com>
2020-06-02 13:15:41 -0700

    Adds doc strings

--- safe_browsing/ifx/configuration.py
+++ safe_browsing/ifx/configuration.py
@@ -1,10 +1,11 @@
-"Configuration functions"
+"""Configuration functions."""
 
 from configparser import ConfigParser
 from pathlib import Path
 
 
 def get_value(key):
+    """Return value from config file for given key."""
     # check local config.ini
     parser = ConfigParser(delimiters='|')  # Make a config parser
     path = Path('config.ini')  # Make a path object
