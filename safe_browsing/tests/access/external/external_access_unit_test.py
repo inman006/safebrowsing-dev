d601782647c322fa179fae4e43fff37beca57d34
Ryan Inman <inman006@gmail.com>
2020-06-04 14:43:54 -0700

    Updates unit tests to include Dacite checks

--- safe_browsing/tests/access/external/external_access_unit_test.py
+++ safe_browsing/tests/access/external/external_access_unit_test.py
@@ -1,33 +1,100 @@
 """This is where you would write your tests."""
 
-
+from dataclasses import dataclass
 import safe_browsing.access.external.service as external_access
+import dacite
+from safe_browsing.access.external.contracts import SafeBrowsingRequest, ThreatTypes
 
 
 def test_safe():  # noqa:  D103
+
     # Arrange ----------------------------------------------------------------
-    url_to_check = 'https://www.google.com'
+
+    payload = dict(
+        malware=True,
+        social_engineering=True,
+    )
+    threat_types = dacite.from_dict(contracts.ThreatTypes, payload)
+
+    threat_entries = [
+        "www.google.com",
+        "www.youtube.com",
+    ]
+
+    payload = dict(
+        threat_types=threat_types,
+        threat_entries=threat_entries,   
+    )
+
+    safe_browsing_request = dacite.from_dict(contracts.SafeBrowsingRequest, payload)
+
     # Act --------------------------------------------------------------------
-    response = external_access.call_api(url_to_check)
+
+    response = external_access.call_api(safe_browsing_request)
+
     # Assert -----------------------------------------------------------------
+
     assert response.ok  # 200 response
     assert not response.json()  # An empty dict is good
 
 
 def test_unsafe():  # noqa:  D103
     # Arrange ----------------------------------------------------------------
-    url_to_check = 'http://malware.testing.google.test/testing/malware/'
+
+    payload = dict(
+        malware=True,
+        social_engineering=True,
+    )
+
+    threat_types = dacite.from_dict(contracts.ThreatTypes, payload)
+
+    threat_entries = [
+        'http://malware.testing.google.test/testing/malware/'
+    ]
+
+    payload = dict(
+        threat_types=threat_types,
+        threat_entries=threat_entries,   
+    )
+
+    safe_browsing_request = dacite.from_dict(contracts.SafeBrowsingRequest, payload)
+
     # Act --------------------------------------------------------------------
-    response = external_access.call_api(url_to_check)
+
+    response = external_access.call_api(safe_browsing_request)
+
     # Assert -----------------------------------------------------------------
+
     assert response.ok  # Still 200 response
     assert response.json()  # The result should not be empty
 
 
 def test_synntax_errer():  # noqa:  D103
+
     # Arrange ----------------------------------------------------------------
-    url_to_check = 0  # This is not a string; i.e., invalid url
+
+    payload = dict(
+        malware=True,
+        social_engineering=True,
+    )
+
+    threat_types = dacite.from_dict(contracts.ThreatTypes, payload)
+
+    threat_entries = [
+        0,  # This is not a string; i.e., invalid url
+    ]
+
+    payload = dict(
+        threat_types=threat_types,
+        threat_entries=threat_entries,   
+    )
+
+    safe_browsing_request = dacite.from_dict(contracts.SafeBrowsingRequest, payload)
+
     # Act --------------------------------------------------------------------
-    response = external_access.call_api(url_to_check)
+
+    response = external_access.call_api(safe_browsing_request)
+
     # Assert -----------------------------------------------------------------
+
     assert not response.ok  # 400 response
