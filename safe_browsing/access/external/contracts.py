6567dc903b0daa4555235e0867c3802fa1dfd76e
Ryan Inman <inman006@gmail.com>
2020-07-22 14:17:46 -0700

    adds SafeBrowsingResponse

--- safe_browsing/access/external/contracts.py
+++ safe_browsing/access/external/contracts.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass
 class ThreatTypes:
     """Threat types to check."""
@@ -10,9 +11,18 @@ class ThreatTypes:
     malware: bool
     social_engineering: bool
 
+
 @dataclass
 class SafeBrowsingRequest:
     """Encapsulate all aspects of a Safe Browsing request."""
 
     threat_types: ThreatTypes
     threat_entries:  List[str]
+
+
+@dataclass
+class SafeBrowsingResponse:
+    """Response to a Safe Browsing request."""
+
+    safe_urls:  List[str]
+    unsafe_urls:  List[str]
