973a5fa0ee065025ae69a8d7cbce189bcf334043
Ryan Inman <inman006@gmail.com>
2020-07-22 14:19:46 -0700

    Updates call_api fucntion using list comprehension

--- safe_browsing/access/external/service.py
+++ safe_browsing/access/external/service.py
@@ -27,8 +27,34 @@ PAYLOAD = {
 }
 
 
-def call_api(url_to_check):
+def call_api(safe_browsing_request):
     """Check url against safe browsing API."""
-    entry = {'url': url_to_check}
-    PAYLOAD['threatInfo']['threatEntries'].append(entry)
-    return requests.post(API_URL, json=PAYLOAD)
+    urls_to_check = safe_browsing_request.threat_entries
+    # Use list comprehension here
+    breakpoint()
+    my_urls = {url: url for urls in urls_to_check}
+    # my_urls = list()
+    # for url in urls_to_check:  # grab one widget and go around the track
+    #     payload = dict(url=url)  # Do something with the widget
+    #     my_urls.append(payload)  # Put the result in the bag and look for the next one
+    PAYLOAD['threatInfo']['threatEntries'] = my_urls
+    response_from_requests = requests.post(API_URL, json=PAYLOAD)
+    # The response only contains information regarding unsafe urls
+    # As a result, we assume all urls in `urls_to_check` are safe until
+    # we see information in the response telling us it is not.
+    safe_urls = list(urls_to_check)  # Make a copy of urls to check
+    response_dict = response_from_requests.json()  # potentially contains info regarding unsafe urls
+    # So go through each thing in the dict using a loop and remove and append
+    # DOn't tryu to use a list comprehension here!
+    unsafe_urls = list()
+    for match in response_dict['matches']:  # debug
+        unsafe_url = match['threat']['url']
+        unsafe_urls.appned(unsafe_url)
+        safe_urls.revove(unsafe_url)
+    payload = dict(
+      safe_urls=safe_urls,
+      unsafe_urls=unsafe_urls,
+      )
+    # use the response_from_requests to build a concretion of a SafeBrowsingResponse
+    return response_from_requests
+    
